f378f8c5404eac58a9ffefb3d00543071bb3f1b1
Nico Schertler <nico.schertler@tu-dresden.de>
2018-03-21 13:03:18 +0100

    non-Windows compatibility

--- libOSR/include/osr/OSRLibrary.h
+++ libOSR/include/osr/OSRLibrary.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #ifndef OSR_EXPORT
+#  ifdef _WIN32
 #    ifdef OSR_EXPORTS
-/* We are building this library */
+       /* We are building this library */
 #      define OSR_EXPORT __declspec(dllexport)
 #    else
-/* We are using this library */
+       /* We are using this library */
 #      define OSR_EXPORT __declspec(dllimport)
 #    endif
+#  else
+#    define OSR_EXPORT
 #  endif
